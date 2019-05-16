4c18df492e3e83ee6761eb76e1b4d9b262bbf99b
github username <github email address>
2019-05-15 20:01:18 -0700

    added the about page and project page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,46 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import HomeCard from './components/HomeCard';
+import ProjectList from './components/ProjectList';
+import AboutMe from './components/AboutMe';
 
 class App extends React.Component{
 
+  state={selectionName: "home"}
+
   handleMenuItem = (ev)=>{
-    console.log("check menuitem:", ev.target.id)
+    this.setState({selectionName: ev.target.id})
   }
 
   render(){
-    return (
-      <div>
-        <NavBar handleMenuItem={this.handleMenuItem}/><br/>
-        <HomeCard handleMenuItem={this.handleMenuItem}/>
-      </div>
-    )
+    if(this.state.selectionName === "home"){
+      return(
+        <div>
+          <NavBar handleMenuItem={this.handleMenuItem}/><br/>
+          <HomeCard handleMenuItem={this.handleMenuItem}/>
+        </div>
+      )}
+      else if(this.state.selectionName === "project"){
+        return(
+          <div>
+            <NavBar handleMenuItem={this.handleMenuItem}/><br/><br/><br/><br/>
+            <ProjectList />
+          </div>
+        )  }
+      else if(this.state.selectionName === "about"){
+        return(
+          <div>
+            <NavBar handleMenuItem={this.handleMenuItem}/><br/><br/><br/>
+            <AboutMe />
+          </div>
+        )}
+      else{
+        return(
+          <div>
+            <NavBar handleMenuItem={this.handleMenuItem}/><br/>
+            <HomeCard handleMenuItem={this.handleMenuItem}/>
+          </div>
+        )}
   }
 }
 export default App;
--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Image, Icon, Popup, Label} from 'semantic-ui-react'
+import {Card, Icon, Popup, Label} from 'semantic-ui-react'
 const HomeCard =(props)=>{
   return(
   <Card  centered fluid id="main_card">
@@ -7,13 +7,14 @@ const HomeCard =(props)=>{
       <Card.Header textAlign="center">Yong Liang</Card.Header>
       <Card.Meta textAlign="center">Software Engineer</Card.Meta>
       <Card.Description textAlign="center">
-        <p className="home_description"> - I'm a junior web developer in Seattle, a technology enthusiast who loves to write codes and build products.</p> <p className="home_description">To learn more about me and my works, please select one of the following:</p>
+        <p className="home_description"> - I'm a web developer in Seattle, a technology enthusiast who loves to write codes and build products.</p> <p className="home_description">To learn more about me and my works, please select one of the following:</p>
         {/*icon selections  */}
-        <Popup trigger={<Label circular color="blue" as='a' onClick={props.handleMenuItem} id="projectIcon">Projects</Label>} content="Visit projects page"/>
-        <Popup trigger={<Label circular color="blue" as='a' onClick={props.handleMenuItem} id="aboutIcon">About</Label>} content="Visit about page"/>
-        <Popup trigger={<Label circular color="blue" as='a' href="https://yongliang24.github.io/resume/" target="_blank"  rel="noopener noreferrer">Resume</Label>} content="Visit resume {External Link}"/>
-        <Popup trigger={<a href="https://github.com/YongLiang24" target="_blank" rel="noopener noreferrer"><Icon name='github' size="big"/></a>} content="Visit github {External Link}"/>
-        <Popup trigger={<a href="https://www.linkedin.com/in/yongliang24/" target="_blank" rel="noopener noreferrer"><Icon name='linkedin' size="big"/></a>} content="Visit linkedin {External Link}"/>
+        <Popup trigger={<Label circular color="blue" as='a' onClick={props.handleMenuItem} id="project">Projects</Label>} content="Visit projects page"/>
+        <Popup trigger={<Label circular color="blue" as='a' onClick={props.handleMenuItem} id="about">About</Label>} content="Visit about page"/>
+        <Popup trigger={<Label circular color="blue" as='a' href="https://yongliang24.github.io/resume/" target="_blank"  rel="noopener noreferrer">Resume</Label>} content="Visit my resume {External Link}"/>
+        <Popup trigger={<Label circular color="blue" as='a' href="https://dev.to/yongliang24" target="_blank"  rel="noopener noreferrer">Blogs</Label>} content="Visit my blogs {External Link}"/>
+        <Popup trigger={<a href="https://github.com/YongLiang24" target="_blank" rel="noopener noreferrer"><Icon name='github' size="big"/></a>} content="Visit my github {External Link}"/>
+        <Popup trigger={<a href="https://www.linkedin.com/in/yongliang24/" target="_blank" rel="noopener noreferrer"><Icon name='linkedin' size="big"/></a>} content="Visit my linkedin {External Link}"/>
       </Card.Description>
     </Card.Content>
   </Card>)
