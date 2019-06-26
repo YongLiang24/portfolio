eb219f366296682f13617e5b40706158fbceff17
Yong Liang <yongliang24@gmail.com>
2019-06-26 14:41:37 -0700

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,7 +7,8 @@ const AboutMe = ()=>{
       <Card centered fluid id="about_card"  color='orange' raised>
         <h2 style={{textAlign: "center"}}>Welcome to My Portfolio Site</h2>
         <Message.Content style={{lineHeight: "1.8", textAlign: "left"}}>
-    A computer enthusiast, loves to code and build things, driven to complete difficult tasks. I enjoy learning new things and that helps me to pick up on required skills quickly.
+    A computer enthusiast, love to build things, driven to complete difficult tasks. 
+    I enjoy learning new things and that helps me to pick up on required skills quickly.
 
       </Card>
     </div>
