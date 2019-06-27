5bae9d1fe8d5c8976d55e6a44bbec5e0bbec87c5
Yong Liang <yongliang24@gmail.com>
2019-06-26 17:15:34 -0700

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,6 +8,7 @@ const AboutMe = ()=>{
         <h2 style={{textAlign: "center"}}>Welcome to My Portfolio Site</h2>
         <Message.Content style={{lineHeight: "1.8", textAlign: "left"}}>
     A computer enthusiast, love to build things, driven to complete difficult tasks. I enjoy learning new things and that helps me to pick up on required skills quickly.
+     </Message.Content>
     </Card>
     </div>
   )
